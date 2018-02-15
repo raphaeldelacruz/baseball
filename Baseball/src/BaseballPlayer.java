60aa4dea46a83244fcaa64336431e78328562e60
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2018-02-15 17:37:37 -0500

    baseball program

---
+++ Baseball/src/BaseballPlayer.java
@@ -0,0 +1,132 @@
+public class BaseballPlayer 
+{
+	private int H;
+	private int aB;
+	private int bB;
+	private int hBP;
+	private int sF;
+	private int dbls;
+	private int triples;
+	private int hR;
+	
+	private int singles;
+	private double bA;
+	private double oBP;
+	private double sLG;
+	private double oPS;
+	private double tB;
+	
+	BaseballPlayer(int hits, int aB, int bB, int hBP, int sF, int dbls, int triples, int hR)
+	{
+		this.H = hits;
+		this.aB = aB;
+		this.bB = bB;
+		this.hBP = hBP;
+		this.sF = sF;
+		this.dbls = dbls;
+		this.triples = triples;
+		this.hR = hR;
+		
+		this.singles = this.H - this.dbls - this.triples - this.hR;
+		this.bA = this.calculateBA();
+		this.oBP = this.calculateOBP();
+		this.sLG = this.calculateSLG();
+		this.oPS = this.calculateOPS();
+		this.tB = this.calculateTB();
+	}
+	
+	public double calculateBA() {
+		return (double)this.H/(double)this.aB;
+	}
+	public double calculateOBP() {
+		return ((double)this.H + (double)this.bB + (double)this.hBP)/((double)this.aB + (double)this.bB + (double)this.hBP + (double)this.sF);
+	}
+	public double calculateSLG() {
+		return (((double)this.singles + (2.0 * (double)this.dbls) + (3.0*(double)this.triples) + (4.0*(double)this.hR))/(double)this.aB);
+	}
+	public double calculateOPS() {
+		return this.oBP + this.sLG;
+	}
+	public double calculateTB() {
+		return (double)this.singles + (2.0*(double)this.dbls) + (3.0*(double)this.triples) + (4.0*(double)this.hR);
+	}
+	public int getaB() {
+		return aB;
+	}
+
+	public void setaB(int aB) {
+		this.aB = aB;
+	}
+
+	public int getH() {
+		return H;
+	}
+
+	public void setH(int h) {
+		H = h;
+	}
+
+	public int getDbls() {
+		return dbls;
+	}
+
+	public void setDbls(int dbls) {
+		this.dbls = dbls;
+	}
+
+	public int getTriples() {
+		return triples;
+	}
+
+	public void setTriples(int triples) {
+		this.triples = triples;
+	}
+
+	public int gethR() {
+		return hR;
+	}
+
+	public void sethR(int hR) {
+		this.hR = hR;
+	}
+
+	public int getbB() {
+		return bB;
+	}
+
+	public void setbB(int bB) {
+		this.bB = bB;
+	}
+
+	public int getsF() {
+		return sF;
+	}
+
+	public void setsF(int sF) {
+		this.sF = sF;
+	}
+
+	public int gethBP() {
+		return hBP;
+	}
+
+	public void sethBP(int hBP) {
+		this.hBP = hBP;
+	}
+
+	public double getBA() {
+		return this.bA;
+	}
+	public double getOBP() {
+		return this.oBP;
+	}
+	public double getSLG() {
+		return this.sLG;
+	}
+	public double getOPS() {
+		return this.oPS;
+	}
+	public double getTB() {
+		return this.tB;
+	}
+}
---
+++ Baseball/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Player Name:");
+		String name = sc.nextLine();
+		System.out.println("Enter Player Hits:");
+		int hits = sc.nextInt();
+		System.out.println("Enter Player At Bats:");
+		int atBats = sc.nextInt();
+		System.out.println("Enter Player Walks:");
+		int walks = sc.nextInt();
+		System.out.println("Enter Player Hits by Pitch:");
+		int hitsbypitch = sc.nextInt();
+		System.out.println("Enter Player Sacrifice Flys:");
+		int sacflies = sc.nextInt();
+		System.out.println("Enter Player Doubles:");
+		int doubles = sc.nextInt();
+		System.out.println("Enter Player Triples:");
+		int triples = sc.nextInt();
+		System.out.println("Enter Player Homeruns:");
+		int hr = sc.nextInt();
+		
+		BaseballPlayer player1 = new BaseballPlayer(hits,atBats,walks,hitsbypitch,sacflies,doubles,triples,hr);
+		
+		// Babe Ruth = 2873, 8399, 2062, 43, 0, 506, 136, 714
+		System.out.println("The Baseball Stats for " + name + " are:");
+		System.out.println("Hits: " + hits + " At Bats: " + atBats + " Walks: " + walks 
+				+ " Hits by Pitch: " + hitsbypitch + " Sacrifice Flies: " + sacflies + " Doubles: " 
+				+ doubles + " Triples: " + triples + " Homeruns: " + hr);
+		System.out.println("Batting Average: " + player1.getBA() + " OBP: " + player1.getOBP() + " SLG: " 
+				+ player1.getSLG() + " OPS: " + player1.getOPS() + " TB: " + player1.getTB());		
+	}
+}
